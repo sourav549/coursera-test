c6d66a9af7f202d9b2e038b8948acd5fe5850a8d
sourav549 <sourav.bit@live.in>
2016-09-25 20:09:58 +0530

    module 2 added

--- practice/app.js
+++ practice/app.js
@@ -1,44 +1,23 @@
 (function(){
-  'use strict'
+  angular.module('customFilterApp',[])
+  .controller('customFilterController',customFilterController)
+  .filter('truth',CustomFilterTruth);
+customFilterController.$inject('$scope','truthFilter');
 
-angular.module('LunchCheckerApp',[])
-
-.controller('LunchCheckerController',LunchCheckerController);
-LunchCheckerController.$inject = ['$scope'];
-
-function LunchCheckerController($scope){
-
-$scope.CountCheck = function(){
-  var len = splitString($scope.foods);
-if (len>3) {
-  $scope.Message='Too much!';
-  $scope.color = 'green';
-}
-else if (len>0 ) {
-    $scope.Message='Enjoy!';
-    $scope.color = 'green';
-  }
-  else {
-    $scope.Message='Please enter data first';
-    $scope.color = 'red';
-  }
-};
-
-  function splitString(str){
-    var count=0;
-    if (str==' ' || str=='' || str==null) {
-      return count;
+  function customFilterController($scope,truthFilter){
+$scope.gameName = "DMC5";
+$scope.realGame = "Dark Souls";
+// $scope.getRealGame = truthFilter("Dark Souls 5");
+  };
+function CustomFilterTruth(){
+  return function(input, partNo){
+    if (partNo == 1) {
+      input = input.replace('Dark Souls','BloodBorne');
     }
-    var array = str.split(',');
-    for (var i = 0; i < array.length; i++) {
-      if (array[i]=='' || array[i]==' ') {
-        //nothing
-      }
-      else {
-        count++;
-      }
+    if (partNo == 2) {
+        input = input.replace('Dark Souls','FireWatch');
     }
-    return count;
+    return input;
   };
 };
 
